e1dabf19eb4310a37d622c728c1b600b025084bf
fatmeme jadidi <fjadidi2001@gmail.com>
2022-07-13 16:09:39 -0400

    initial files

---
+++ 1package.go
@@ -0,0 +1,12 @@
+//The main function should reside in the main package.
+//the first line of every go source file=>package packagename
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("easy")
+}
+
+//packagename.FunctionName() is the syntax to call a function in a package
+//fmt package to use the Println function. The fmt is a standard package and is available inbuilt as a part of the Go standard library
---
+++ module.go
@@ -0,0 +1,10 @@
+// the import path for the custom package we create is derived from the name of the go module
+package main
+
+func init() {
+
+}
+
+// The init function must not have any return type and it must not have any parameters.
+//The init function cannot be called explicitly in our source code.
+//It will be called automatically when the package is initialized.
